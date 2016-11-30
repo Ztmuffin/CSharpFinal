d7804a35b29c2542dbe2c7fa23123eb0a6d9558e
Ztmuffin <addisonbmoyer@gmail.com>
2016-11-30 15:48:37 -0700

    changed lifes to lives

--- scripts/GameScripts/EndGame.cs
+++ scripts/GameScripts/EndGame.cs
@@ -2,16 +2,16 @@ using System;
 
 
 public class EndGame{
-    int lifes;
+    int lives;
 
     void Awake(){
-        lifes = 5;
+        lives = 5;
     }
 
     public void looseLife(){
-        if (lifes != 0){
-            lifes--;
-        Console.WriteLine ("you have " + lifes + " lives left.");
+        if (lives != 0){
+            lives--;
+        Console.WriteLine ("you have " + lives + " lives left.");
         }
         else {
             GameStatesBase.currentGamestatus = GameStatesBase.GameStatuses.End;
